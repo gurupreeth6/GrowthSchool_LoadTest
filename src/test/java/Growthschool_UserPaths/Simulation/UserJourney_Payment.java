4715bcf80d7b192da36c6c429238654f988a2748
gurupreeth6 <guru7karnik@gmail.com>
2024-04-24 12:05:25 +0530

    Create UserJourney_Payment.java

---
+++ src/test/java/Growthschool_UserPaths/Simulation/UserJourney_Payment.java
@@ -0,0 +1,16 @@
+package Growthschool_UserPaths.Simulation;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import java.time.Duration;
+import Growthschool_UserPaths.Scenarios.*;
+import static io.gatling.javaapi.core.CoreDsl.*;
+
+public class UserJourney_Payment {
+
+    private static final Duration LOW_PAUSE = Duration.ofMillis(1000);
+
+    public static ChainBuilder CasualVisit =
+            exec(PaymentAPI.PaymentLink)
+            .pause(LOW_PAUSE)
+            .exec(PaymentAPI.PaymentLead);
+}
