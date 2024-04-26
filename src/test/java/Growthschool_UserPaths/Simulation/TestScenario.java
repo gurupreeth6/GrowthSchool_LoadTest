39ec707c7368f95735c02b1d237e6ddbc49e6cdd
gurupreeth6 <guru7karnik@gmail.com>
2024-04-26 12:25:40 +0530

    Update TestScenario.java
    Updated the duration to 15 sec

--- src/test/java/Growthschool_UserPaths/Simulation/TestScenario.java
+++ src/test/java/Growthschool_UserPaths/Simulation/TestScenario.java
@@ -6,7 +6,7 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import java.time.Duration;
 public class TestScenario {
 
-    private static final Duration TEST_DURATION = Duration.ofSeconds(Integer.parseInt(System.getProperty("DURATION", "30")));
+    private static final Duration TEST_DURATION = Duration.ofSeconds(Integer.parseInt(System.getProperty("DURATION", "15")));
     public static ScenarioBuilder webinarLoadTest =
             scenario("Webinar Load Test")
                     .during(TEST_DURATION)
