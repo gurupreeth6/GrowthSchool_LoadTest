b63994b40eff2ad5e463df98cb088f4998a9cc0f
gurupreeth6 <guru7karnik@gmail.com>
2024-04-28 10:17:56 +0530

    Update UserJourney_Payment.java
    Added delay between calls

--- src/test/java/Growthschool_UserPaths/Simulation/UserJourney_Payment.java
+++ src/test/java/Growthschool_UserPaths/Simulation/UserJourney_Payment.java
@@ -7,10 +7,16 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class UserJourney_Payment {
 
-    // private static final Duration LOW_PAUSE = Duration.ofMillis(1000);
+    private static final Duration LOW_PAUSE = Duration.ofMillis(2000);
     private static final Duration HIGH_PAUSE = Duration.ofSeconds(15);
 
     public static ChainBuilder CasualVisit =
-            exec(PaymentAPI.PaymentLink,PaymentAPI.PaymentLead,PaymentAPI.PaymentCheckout,PaymentAPI.DiscountCodes, PaymentAPI.Orders)
+            exec(PaymentAPI.PaymentLink)
+            .pause(LOW_PAUSE)
+            .exec(PaymentAPI.PaymentLead,PaymentAPI.PaymentCheckout)
+            .pause(LOW_PAUSE)
+            .exec(PaymentAPI.DiscountCodes)
+            .pause(LOW_PAUSE)
+            .exec(PaymentAPI.Orders)
             .pace(HIGH_PAUSE);
 }
