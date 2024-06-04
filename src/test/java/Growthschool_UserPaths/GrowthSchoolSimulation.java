1822a5184f3c107d95d1fed85a7ccb7e23f833a1
gurupreeth6 <guru7karnik@gmail.com>
2024-06-04 17:10:40 +0530

    Update GrowthSchoolSimulation.java
    Added headers

--- src/test/java/Growthschool_UserPaths/GrowthSchoolSimulation.java
+++ src/test/java/Growthschool_UserPaths/GrowthSchoolSimulation.java
@@ -14,6 +14,10 @@ public class GrowthSchoolSimulation extends Simulation{
 
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://" +DOMAIN)
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") 
+            .acceptEncodingHeader("gzip, deflate")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
             .authorizationHeader("")
             .useAllLocalAddresses();
     {
